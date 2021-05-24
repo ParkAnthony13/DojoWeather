b8e9ff4dcdc06232e4fecc829291cab2d73bc247
Anthony Park <anth0nypark13@gmail.com>
2021-05-24 16:05:19 -0700

    First Upload Includes Challenge

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-var colors = document.querySelectorAll(".color");
+var temps = document.querySelectorAll(".highs,.lows");
+var unit = 'Fahrenheit';
+var cookieBox = document.querySelector(".cookies");
+function wait() {
+    setTimeout(reveal, 1000); /* to have function wait take out parenthesis so you don't call immediately  */
+}
+function reveal() {
+    cookieBox.style.display = 'flex';
+}
+wait();
 
-function applyColors(){
-    for(var i=0; i<colors.lenght; i++) {
-        colors[i].style.color = colors[i].innerText;
+function convertUnits(element) {
+    unit = element.value;
+    console.log(unit);
+    if (unit == 'Celcius') {
+        for (i = 0; i<temps.length; i++) {
+            temps[i].innerHTML = Math.round(((temps[i].innerHTML - 32) * 5 / 9)*10)/10;
+        }
+    } else if (unit == 'Fahrenheit') {
+        for (i = 0; i<temps.length; i++) {
+            temps[i].innerHTML = Math.round(((temps[i].innerHTML * 9 / 5) + 32)*10)/10;
+        }
     }
 }
